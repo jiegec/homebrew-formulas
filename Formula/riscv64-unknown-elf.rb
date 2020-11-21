1817c885c578a63533b2ca6cb6e68f811fac02ce
Jiajie Chen <c@jia.je>
2020-11-21 23:38:39 +0800

    Bump riscv64-unknown-elf

--- Formula/riscv64-unknown-elf.rb
+++ Formula/riscv64-unknown-elf.rb
@@ -1,9 +1,9 @@
 class Riscv64UnknownElf < Formula
   desc "Bare-metal RISCV64 ELF toolchain and cross-compiler for OS X"
   homepage "https://www.sifive.com/boards"
-  url "https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.2.0-2019.05.3-x86_64-apple-darwin.tar.gz"
-  version "8.2.0-2019.05.3"
-  sha256 "027bea47be0af509ea5f7615858f11bb92cf0a9a2a1a5978a92c329592baf73c"
+  url "https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2020.04.0-x86_64-apple-darwin.tar.gz"
+  version "8.3.0-2020.04.0"
+  sha256 "f7b5d1ff92b1fe0db90295ba02e8f684c5d8fc3c26d01cef6b376668053fc038"
 
   def install
     (prefix/"toolchain").install Dir["./*"]
