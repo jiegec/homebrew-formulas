f0dba4146cb29c1844cc33dcaab36f53c907946b
Jiajie Chen <c@jia.je>
2021-01-03 21:51:34 +0800

    Add ngspice-shared formula

---
+++ Formula/ngspice-shared.rb
@@ -0,0 +1,55 @@
+class NgspiceShared < Formula
+  desc "Spice circuit simulator"
+  homepage "https://ngspice.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/33/ngspice-33.tar.gz"
+  sha256 "b99db66cc1c57c44e9af1ef6ccb1dcbc8ae1df3e35acf570af578f606f8541f1"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/ngspice[._-]v?(\d+(?:\.\d+)*)\.t}i)
+  end
+
+  head do
+    url "https://git.code.sf.net/p/ngspice/ngspice.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "bison" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "fftw"
+  depends_on "readline"
+
+  def install
+    system "./autogen.sh" if build.head?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-readline=yes
+      --enable-xspice
+      --without-x
+      --with-ngshared
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cir").write <<~EOS
+      RC test circuit
+      v1 1 0 1
+      r1 1 2 1
+      c1 2 0 1 ic=0
+      .tran 100u 100m uic
+      .control
+      run
+      quit
+      .endc
+      .end
+    EOS
+    system "#{bin}/ngspice", "test.cir"
+  end
+end
