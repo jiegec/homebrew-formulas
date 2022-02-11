15e85ca57d4da0351dd47ebf151c335c90f3d54a
Jiajie Chen <c@jia.je>
2022-02-11 16:53:15 +0800

    Binutils 2.38 for other formulae

--- Formula/mipsel-elf-binutils.rb
+++ Formula/mipsel-elf-binutils.rb
@@ -1,10 +1,10 @@
 class MipselElfBinutils < Formula
   desc "GNU Binutils targetting mipsel-elf"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://sourceware.org/pub/binutils/releases/binutils-2.32.tar.xz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.gz"
-  version "2.32"
-  sha256 "0ab6c55dd86a92ed561972ba15b9b70a8b9f75557f896446c82e8b36e473ee04"
+  url "https://sourceware.org/pub/binutils/releases/binutils-2.38.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.38.tar.xz"
+  version "2.38"
+  sha256 "e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024"
 
   def install
     mkdir "binutils-build" do
